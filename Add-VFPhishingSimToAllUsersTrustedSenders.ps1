7d4951bb11dc7b4a23b123da3a647923022863c1
Bahusafoo <huggans.sean@gmail.com>
2025-08-19 17:07:46 -0500

    25.8.19.1
    Adding resources

---
+++ Add-VFPhishingSimToAllUsersTrustedSenders.ps1
@@ -0,0 +1,38 @@
+﻿#################################################
+# Add-VFPhishingSimToAllUsersTrustedSenders.ps1 #
+# Adds VisuaFUSION Phishing Simulator emails    #
+# to All Mailboxes' trusted Senders Listes      #
+#################################################
+# Author(s): Sean Huggans
+$ScriptVersion = "24.11.14.3"
+
+#################################################
+# Variables
+#######################################
+
+#################################################
+# Functions
+#######################################
+
+#################################################
+# Execution Logic
+#######################################
+if (!(Import-Module ExchangeOnlineManagement)) {
+    Install-Module ExchangeOnlineManagement
+    Import-Module ExchangeOnlineManagement
+}
+
+Connect-ExchangeOnline
+
+$AllMailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited; 
+$AllMailboxes.address
+foreach ($Mailbox in $AllMailboxes) {
+    $MailboxAddress = ""
+    $MailboxAddress = $($Mailbox.EmailAddresses | Where-Object {$_ -clike "SMTP:*"}).Replace("SMTP:","").Trim()
+    if ($MailboxAddress -ne "") {
+        Write-Host $MailboxAddress
+        Set-MailboxJunkEmailConfiguration -Identity $MailboxAddress -TrustedSendersAndDomains @{ add="visuafusion.com","accountmanage.net","auth-online.net","direct-auth.com","office-authenticate.com","tax-check.net","test.accountmanage.net","test.auth-online.net","test.direct-auth.com","test.office-authenticate.com","test.tax-check.net","dev.accountmanage.net","dev.auth-online.net","dev.direct-auth.com","dev.office-authenticate.com","dev.tax-check.net" }
+    }
+}
+
+
---
+++ Export-UsersForPhishingSim.ps1
@@ -0,0 +1,27 @@
+﻿############################################
+# Export-UsersForPhishingSim.ps1
+# Author(s): Sean Huggans
+$ScriptVersion = "24.11.15.2"
+############################################
+# Script will generate CSV of users intended for import into the visuaFUSION Phishing Simulator.
+# first_name,last_name,email,phone
+
+############################################
+# Script Variables
+################################
+$StandardUserOUdn = "OU=Standard Users,OU=DHC-Users,DC=DHC,DC=demonstrationhealth,DC=com" # DN of the OU you wish to export list of users from
+
+############################################
+# Script Execution Logic
+################################
+
+$ExportPath = "$($env:USERPROFILE)\Documents\UserExport-$(Get-Date -format 'yyyyMMdd-HHmmss').csv"
+"first_name,last_name,email,phone" | Out-File -FilePath $ExportPath -Encoding utf8 -Append
+foreach ($ADUser in $(Get-ADUser -Filter {Enabled -eq $true} -SearchBase $StandardUserOUdn -SearchScope Subtree -Properties *)) {
+    if (($ADUser.mail) -and ($ADUser.surName) -and ($ADUser.givenName)) {
+        "$($ADUser.givenName),$($ADUser.surName),$($ADUser.mail),$($ADUser.OfficePhone)" | Out-File -FilePath $ExportPath -Encoding utf8 -Append
+    } else {
+        Write-Warning "$($ADUser.Name) ($($ADUser.SamAccountName)) is missing a first name, last name, or mail attribute and could not be added!"
+    }
+}
+
